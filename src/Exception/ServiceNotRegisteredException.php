887b4b905fed3a139294246cced7bcb67c268b92
Oleksii Bulba <oleksii_bulba@epam.com>
2023-01-08 17:34:44 +0400

    Chore: Enhancement
    - Added an exception message to ServiceNotRegisteredException exception;
    - Added a property `serviceId` to ServiceNotRegisteredException;
    - Added .gitattributes files to avoid unused files in vendor folder;
    
    Signed-off-by: Oleksii Bulba <oleksii_bulba@epam.com>

--- src/Exception/ServiceNotRegisteredException.php
+++ src/Exception/ServiceNotRegisteredException.php
@@ -7,4 +7,17 @@ use Psr\Container\NotFoundExceptionInterface;
 class ServiceNotRegisteredException extends \RuntimeException
     implements NotFoundExceptionInterface
 {
+    private string $serviceId;
+
+    public function __construct(string $serviceId, int $code = 0, ?\Throwable $previous = null)
+    {
+        $this->serviceId = $serviceId;
+
+        parent::__construct(sprintf('Service "%s" not registered.', $this->serviceId), $code, $previous);
+    }
+
+    public function getServiceId(): string
+    {
+        return $this->serviceId;
+    }
 }
