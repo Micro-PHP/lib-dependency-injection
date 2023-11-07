ea538b201120ff30adf76c785cfa14428295481a
Stanislau Komar <stanislau_komar@epam.com>
2023-11-07 17:38:36 +0400

    v1.6.4 Allow forced override services

--- src/Exception/ServiceNotRegisteredException.php
+++ src/Exception/ServiceNotRegisteredException.php
@@ -17,7 +17,7 @@ class ServiceNotRegisteredException extends \RuntimeException implements NotFoun
 {
     private string $serviceId;
 
-    public function __construct(string $serviceId, int $code = 0, ?\Throwable $previous = null)
+    public function __construct(string $serviceId, int $code = 0, \Throwable $previous = null)
     {
         $this->serviceId = $serviceId;
 
